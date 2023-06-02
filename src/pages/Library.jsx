a992609f65a86c57429e6b09535e8bd6f51c8a91
JivkoKelchev <jivko.r.kelchev@gmail.com>
2023-06-02 08:39:26 +0300

    add book form

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -4,6 +4,7 @@ import {useSigner} from "wagmi";
 import libraryABI from '../abi/Library.json';
 import BookCard from "../components/ui/BookCard";
 import Loading from "../components/ui/Loading";
+import Button from "../components/ui/Button";
 
 
 
@@ -17,6 +18,16 @@ const Library = () => {
   const { data: signer } = useSigner();
   const contractAddress = '0xf071786c9Ab0585d64b0E53d7d027B3E30310324';
 
+  const initialFormData = {
+    title: '',
+    author: '',
+    copies: 0
+  };
+
+  // Form states
+  const [addBookFromData, setAddBookFormData] = useState(initialFormData);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
   const getContractData = useCallback(async () => {
     setIsLoadingContractData(true);
 
@@ -28,6 +39,7 @@ const Library = () => {
     }));
 
     let currentBooks = new Map();
+    let isOwner = false;
     if(signer) {
       const signerAddress = await signer.getAddress();
       const currentBooksId = await contract.showUserCurrentBooks(signerAddress);
@@ -35,9 +47,13 @@ const Library = () => {
         let book = await contract.books(bookId);
         currentBooks.set(bookId, book);
       }));
+
+      //check if user is owner
+      const owner = await contract.owner()
+      isOwner = owner === signerAddress;
    }
 
-    setContractData({ availableBooks, currentBooks });
+    setContractData({ availableBooks, currentBooks,  isOwner});
     setIsLoadingContractData(false);
   }, [contract, signer]);
 
@@ -91,30 +107,101 @@ const Library = () => {
     }
   };
 
-  const handleErrorClick = (e) => {
+  const handleFormInputChange = e => {
+    const { value, name } = e.target;
+
+    setAddBookFormData(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitButtonClick = async () => {
+    try {
+      const { title, author, copies } = addBookFromData;
+
+      const tx = await contract.addBook(title, author, copies);
+      await tx.wait();
+
+      setAddBookFormData(initialFormData);
+
+      await getContractData();
+    } catch (e) {
+      setError(e.reason);
+    } finally {
+      setIsFormVisible(false);
+    }
+  };
+
+  const handleAddBookButtonClick = () => {
+    setIsFormVisible(true);
+  }
+
+  const handleErrorClick = () => {
 
     let hasError = false;
     let error = '';
     setError( {hasError, error});
   }
 
+  const handleFormCloseClick = () => {
+    setIsFormVisible( false);
+  }
+
+  const handleFormClick = (e) => {
+    e.stopPropagation();
+  }
+
   return(
     <div className="container my-5">
       {error.hasError ?
         <div className="error-container" onClick={handleErrorClick}>
-          <div className="error-msg" onBlur={handleErrorClick}> {error.error} </div>
+          <div className="error-msg"> {error.error} </div>
         </div>
         : null}
+      {isFormVisible ?
+        <div className="form-background" onClick={handleFormCloseClick}>
+
+        <div className="form-container" onClick={handleFormClick}>
+          <h3 className="text-headline mb-4">Add new book</h3>
+          <label htmlFor="title">Title:</label>
+          <input
+            type="text"
+            className="form-control"
+            name="title"
+            onChange={handleFormInputChange}
+          />
+          <label htmlFor="author">Author:</label>
+          <input
+            type="text"
+            className="form-control"
+            name="author"
+            onChange={handleFormInputChange}
+          />
+          <label htmlFor="title">Copies:</label>
+          <input
+            type="number"
+            className="form-control"
+            name="copies"
+            onChange={handleFormInputChange}
+          />
+          <Button onClick={handleSubmitButtonClick}>Submit</Button>
+        </div>
+        </div>: null
+      }
       {isLoadingContractData ? (
         <Loading />
       ) : (
       <div>
 
+        <div id="add-book">
           <h3 className="text-headline mb-4">Available books</h3>
-          <div className="row">
+          {contractData.isOwner ? <Button onClick={handleAddBookButtonClick} type='secondary'>Add book</Button> : null}
+        </div>
+          <div className="shelve">
             { [...contractData.availableBooks.keys()].map((key) => {
               return (
-                <div key={ 'available' + key } className='col-3'>
+                <div key={ 'available' + key } className='book'>
                 <BookCard
                   bookId={key}
                   title={contractData.availableBooks.get(key)[0]}
@@ -127,13 +214,13 @@ const Library = () => {
           })}
           </div>
 
-        <hr className="mt-3 mb-6"/>
+        <br/>
 
           <h3 className="text-headline mb-4">My books</h3>
-          <div className="row">
+          <div className="shelve">
             { [...contractData.currentBooks.keys()].map((key) => {
               return (
-                <div key={ key } className='col-3'>
+                <div key={ key } className='book'>
                   <BookCard
                     bookId={key}
                     title={contractData.currentBooks.get(key)[0]}
